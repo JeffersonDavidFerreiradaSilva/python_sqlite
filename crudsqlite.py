549a365ae82e1153cdd77624dfbd838c24aadf0b
Jefferson David <napsterdavid@gmail.com>
2023-08-23 09:13:10 -0300

    foi criado um bloco para gerar uma tabela no banco de dado caso não exista na tabela.

--- crudsqlite.py
+++ crudsqlite.py
@@ -6,5 +6,18 @@ BD_FILE  = ROOT_DIR/BD_NAME
 TABLE_NAME = 'tb_alunos'
 conexao = sqlite3.connect(BD_FILE)
 cursor  = conexao.cursor()
+
+cursor.execute(f"CREATE TEBLE IF NO EXISTS {TABLE_NAME}"
+               '('
+               'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+               'nome TEXT NOT NULL,'
+               'idaed INTEGER NOT NULL,'
+               'peso REAL NOT NULL'
+               ')'
+               )
+conexao.commit()
+
+
+
 cursor.close()
 conexao.close()
